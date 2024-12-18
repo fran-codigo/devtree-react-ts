ab55da444020eac7eb13767821ae58fd660671f8
Franco <franco_tigres18@hotmail.com>
2024-12-18 16:10:51 -0600

    feat|client axios add

--- src/views/RegisterView.tsx
+++ src/views/RegisterView.tsx
@@ -1,8 +1,9 @@
 import { Link } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
-import axios, { isAxiosError } from 'axios';
+import { isAxiosError } from 'axios';
 import { toast } from 'sonner';
 import type { RegisterForm } from '../types';
+import api from '../config/axios';
 import ErrorMessage from '../components/ErrorMessage';
 
 export default function RegisterView() {
@@ -26,10 +27,7 @@ export default function RegisterView() {
 
   const handleRegister = async (formData: RegisterForm) => {
     try {
-      const { data } = await axios.post(
-        `${import.meta.env.VITE_API_URL}/auth/register`,
-        formData
-      );
+      const { data } = await api.post('/auth/register', formData);
       toast.success(data.msg);
       reset();
     } catch (error) {

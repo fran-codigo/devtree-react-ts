aa405ff6a919621c9d9553a69d5bd443554ecd72
Franco <franco_tigres18@hotmail.com>
2024-12-18 14:19:41 -0600

    style|login adn register view header

--- src/views/LoginView.tsx
+++ src/views/LoginView.tsx
@@ -3,10 +3,15 @@ import { Link } from 'react-router-dom';
 export default function LoginView() {
   return (
     <>
-      
+      <h1 className="text-4xl text-white font-bold">Iniciar Sesión</h1>
 
-      <nav>
-        <Link to="/auth/registrar">¿No tienes cuenta? Crea una</Link>
+      <nav className="mt-10">
+        <Link
+          to="/auth/registrar"
+          className="text-center text-white text-lg block"
+        >
+          ¿No tienes cuenta? Crea una
+        </Link>
       </nav>
     </>
   );
--- src/views/RegisterView.tsx
+++ src/views/RegisterView.tsx
@@ -3,9 +3,15 @@ import { Link } from 'react-router-dom';
 export default function RegisterView() {
   return (
     <>
-      <div className="">Register View</div>
-      <nav>
-        <Link to="/auth/login">¿Ya tienes cuenta? Inicia Sesión</Link>
+      <h1 className="text-4xl text-white font-bold">Crear Cuenta</h1>
+
+      <nav className="mt-10">
+        <Link
+          to="/auth/login"
+          className="text-center text-white text-lg block"
+        >
+          ¿Ya tienes cuenta? Inicia Sesión
+        </Link>
       </nav>
     </>
   );

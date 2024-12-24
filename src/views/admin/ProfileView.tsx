a38ffbc25443cfd6ebf10e75634a27a37747fb51
Franco <franco_tigres18@hotmail.com>
2024-12-24 17:42:57 -0600

    feat|update info profile with useMutation

--- src/views/admin/ProfileView.tsx
+++ src/views/admin/ProfileView.tsx
@@ -1,7 +1,9 @@
 import { useForm } from 'react-hook-form';
-import { useQueryClient } from '@tanstack/react-query';
+import { useQueryClient, useMutation } from '@tanstack/react-query';
+import { toast } from 'sonner';
 import ErrorMessage from '../../components/ErrorMessage';
 import { ProfileForm, User } from '../../types';
+import { updateProfile } from '../../api/DevTreeAPI';
 
 export default function ProfileView() {
   const queryClient = useQueryClient();
@@ -18,8 +20,18 @@ export default function ProfileView() {
     },
   });
 
+  const updateProfileMutation = useMutation({
+    mutationFn: updateProfile,
+    onError: (error) => {
+      toast.error(error.message);
+    },
+    onSuccess: (data) => {
+      toast.success(data);
+    },
+  });
+
   const handleUserProfileForm = (formData: ProfileForm) => {
-    console.log(formData);
+    updateProfileMutation.mutate(formData);
   };
 
   return (

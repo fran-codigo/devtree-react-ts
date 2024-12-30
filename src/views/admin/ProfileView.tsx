1645c99163e6b998edd63cfa1854e0cf96de357d
Franco <franco_tigres18@hotmail.com>
2024-12-29 20:10:27 -0600

    feat|upload an image

--- src/views/admin/ProfileView.tsx
+++ src/views/admin/ProfileView.tsx
@@ -3,7 +3,7 @@ import { useQueryClient, useMutation } from '@tanstack/react-query';
 import { toast } from 'sonner';
 import ErrorMessage from '../../components/ErrorMessage';
 import { ProfileForm, User } from '../../types';
-import { updateProfile } from '../../api/DevTreeAPI';
+import { updateProfile, uploadImage } from '../../api/DevTreeAPI';
 
 export default function ProfileView() {
   const queryClient = useQueryClient();
@@ -30,6 +30,22 @@ export default function ProfileView() {
       queryClient.invalidateQueries({ queryKey: ['user'] });
     },
   });
+
+  const uploadImageMutation = useMutation({
+    mutationFn: uploadImage,
+    onError: (error) => {
+      console.log(error);
+    },
+    onSuccess: (data) => {
+      console.log(data);
+    },
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files) {
+      uploadImageMutation.mutate(e.target.files[0]);
+    }
+  };
 
   const handleUserProfileForm = (formData: ProfileForm) => {
     updateProfileMutation.mutate(formData);
@@ -74,7 +90,7 @@ export default function ProfileView() {
           name="handle"
           className="border-none bg-slate-100 rounded-lg p-2"
           accept="image/*"
-          onChange={() => {}}
+          onChange={handleChange}
         />
       </div>
 

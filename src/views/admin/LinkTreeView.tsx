396a70d92ddaba10911ae2d841e6312c7a76ec83
Franco <franco_tigres18@hotmail.com>
2025-01-17 16:33:31 -0600

    feat|LinkTreeView:component DevtreeInput add with style

--- src/views/admin/LinkTreeView.tsx
+++ src/views/admin/LinkTreeView.tsx
@@ -1,9 +1,20 @@
+import DevTreeInput from '../../components/DevTreeInput';
 import { social } from '../../data/social';
 import { useState } from 'react';
 
 export default function LinkTree() {
   const [devTreeLinks, setDevTreeLinks] = useState(social);
-  
 
-  return <div>LinkTree</div>;
+  return (
+    <>
+      <div className="space-y-5">
+        {devTreeLinks.map((item) => (
+          <DevTreeInput
+            key={item.name}
+            item={item}
+          />
+        ))}
+      </div>
+    </>
+  );
 }
